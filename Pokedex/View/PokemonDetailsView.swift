ad739e75d7e5a2bf7ba13c52b66b7c4a68b49978
Erik M <muzsi.erik@agilefreaks.com>
2026-04-02 15:19:26 +0300

    Extract image view to separate file

--- Pokedex/View/PokemonDetailsView.swift
+++ Pokedex/View/PokemonDetailsView.swift
@@ -5,7 +5,6 @@
 //  Created by Freak on 25.02.2026.
 //
 
-import SDWebImageSwiftUI
 import SwiftUI
 
 struct PokemonDetailsView: View {
@@ -46,43 +45,7 @@ struct PokemonDetailsView: View {
     }
 }
 
-struct PokemonImageView: View {
-    @State private var didFail = false
-    let spriteURL: URL?
-    var body: some View {
-        ZStack {
-            Circle()
-                .fill(Color.blue.opacity(0.1))
-                .frame(width: 260, height: 260)
 
-            if didFail {
-                Image("missingno")
-                    .resizable()
-                    .frame(width: 250, height: 250)
-            } else {
-                WebImage(url: spriteURL)
-                    .onSuccess { image, data, cacheType in
-                        print(
-                            "Loaded from: \(cacheType == .disk ? "Disk" : "Network")"
-                        )
-                    }
-                    .onFailure { error in
-                        print(
-                            "Image failed to load: \(error.localizedDescription)"
-                        )
-                        DispatchQueue.main.async {
-                            self.didFail = true
-                        }
-                    }
-                    .resizable()
-                    .indicator(.activity)
-                    .frame(width: 250, height: 250)
-            }
-
-        }
-        .padding(.top, 30)
-    }
-}
 
 struct PokemonInfoHeaderView: View {
     let id: Int
