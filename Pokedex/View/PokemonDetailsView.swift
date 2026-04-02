3105d8861b9485a488df21664c43ae99db779845
Erik M <muzsi.erik@agilefreaks.com>
2026-04-02 14:58:42 +0300

    Extract image header to its own view

--- Pokedex/View/PokemonDetailsView.swift
+++ Pokedex/View/PokemonDetailsView.swift
@@ -12,43 +12,11 @@ struct PokemonDetailsView: View {
     let pokemon: Pokemon
     let isFavorite: Bool
     let onFavoriteToggle: () -> Void
-    @State private var didFail = false
 
     var body: some View {
         ScrollView {
             VStack(spacing: 20) {
-                //Image Header
-                ZStack {
-                    Circle()
-                        .fill(Color.blue.opacity(0.1))
-                        .frame(width: 260, height: 260)
-
-                    if didFail {
-                        Image("missingno")
-                            .resizable()
-                            .frame(width: 250, height: 250)
-                    } else {
-                        WebImage(url: pokemon.spriteURL)
-                            .onSuccess { image, data, cacheType in
-                                print(
-                                    "Loaded from: \(cacheType == .disk ? "Disk" : "Network")"
-                                )
-                            }
-                            .onFailure { error in
-                                print(
-                                    "Image failed to load: \(error.localizedDescription)"
-                                )
-                                DispatchQueue.main.async {
-                                    self.didFail = true
-                                }
-                            }
-                            .resizable()
-                            .indicator(.activity)
-                            .frame(width: 250, height: 250)
-                    }
-
-                }
-                .padding(.top, 30)
+                PokemonImageView(spriteURL: pokemon.spriteURL)
 
                 //ID, name, favorite, generation
                 VStack(spacing: 8) {
@@ -149,6 +117,44 @@ struct PokemonDetailsView: View {
         .navigationTitle(pokemon.name.capitalized)
         .navigationBarTitleDisplayMode(.inline)
         .toolbar(.hidden, for: .tabBar)
+    }
+}
+
+struct PokemonImageView: View {
+    @State private var didFail = false
+    let spriteURL: URL?
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.blue.opacity(0.1))
+                .frame(width: 260, height: 260)
+
+            if didFail {
+                Image("missingno")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+            } else {
+                WebImage(url: spriteURL)
+                    .onSuccess { image, data, cacheType in
+                        print(
+                            "Loaded from: \(cacheType == .disk ? "Disk" : "Network")"
+                        )
+                    }
+                    .onFailure { error in
+                        print(
+                            "Image failed to load: \(error.localizedDescription)"
+                        )
+                        DispatchQueue.main.async {
+                            self.didFail = true
+                        }
+                    }
+                    .resizable()
+                    .indicator(.activity)
+                    .frame(width: 250, height: 250)
+            }
+
+        }
+        .padding(.top, 30)
     }
 }
 
