9c8cd07fde44a6b10b27925e5c514ef3d4118b73
Erik M <muzsi.erik@agilefreaks.com>
2026-04-02 15:06:54 +0300

    Extract pokemon header into its own view

--- Pokedex/View/PokemonDetailsView.swift
+++ Pokedex/View/PokemonDetailsView.swift
@@ -17,45 +17,13 @@ struct PokemonDetailsView: View {
         ScrollView {
             VStack(spacing: 20) {
                 PokemonImageView(spriteURL: pokemon.spriteURL)
-
-                //ID, name, favorite, generation
-                VStack(spacing: 8) {
-                    Text("#\(String(format: "%03d", pokemon.id))")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.secondary)
-
-                    VStack(spacing:10) {
-                        HStack {
-                            Text(pokemon.name.capitalized)
-                                .font(
-                                    .system(
-                                        size: 44,
-                                        weight: .black,
-                                        design: .rounded
-                                    )
-                                )
-                            Button {
-                                onFavoriteToggle()
-                            } label: {
-                                Image(
-                                    systemName: isFavorite
-                                        ? "heart.fill" : "heart"
-                                )
-                                .font(.system(size: 30))
-                                .foregroundStyle(isFavorite ? .red : .gray)
-                            }
-                            .buttonStyle(.borderless)
-                        }
-                        Text(pokemon.formattedGeneration)
-                            .font(.system(size: 20))
-                            .padding(.horizontal, 8)
-                            .padding(.vertical, 4)
-                            .background(.ultraThinMaterial)
-                            .clipShape(Capsule())
-                    }
-                }
-
+                PokemonInfoHeaderView(
+                    id: pokemon.id,
+                    name: pokemon.name,
+                    onFavoriteToggle: onFavoriteToggle,
+                    isFavorite: isFavorite,
+                    formattedGeneration: pokemon.formattedGeneration
+                )
                 //Info Stats
                 HStack(spacing: 40) {
                     StatVStack(
@@ -155,6 +123,54 @@ struct PokemonImageView: View {
 
         }
         .padding(.top, 30)
+    }
+}
+
+struct PokemonInfoHeaderView: View {
+    let id: Int
+    let name: String
+    let onFavoriteToggle: () -> Void
+    let isFavorite: Bool
+    let formattedGeneration: String
+
+    var body: some View {
+        VStack(spacing: 8) {
+            Text("#\(String(format: "%03d", id))")
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundStyle(.secondary)
+
+            VStack(spacing: 10) {
+                HStack {
+                    Text(name.capitalized)
+                        .font(
+                            .system(
+                                size: 44,
+                                weight: .black,
+                                design: .rounded
+                            )
+                        )
+                    Button {
+                        onFavoriteToggle()
+                    } label: {
+                        Image(
+                            systemName: isFavorite
+                                ? "heart.fill" : "heart"
+                        )
+                        .font(.system(size: 30))
+                        .foregroundStyle(isFavorite ? .red : .gray)
+                    }
+                    .buttonStyle(.borderless)
+                }
+                Text(formattedGeneration)
+                    .font(.system(size: 20))
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+                    .background(.ultraThinMaterial)
+                    .clipShape(Capsule())
+            }
+        }
+
     }
 }
 
