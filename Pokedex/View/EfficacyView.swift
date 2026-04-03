1e78bc6cbb2d1a458ed0bed4dfd5833e3de02a37
Erik M <muzsi.erik@agilefreaks.com>
2026-04-03 15:23:48 +0300

    Extract efficacy view to separate file

---
+++ Pokedex/View/EfficacyView.swift
@@ -0,0 +1,48 @@
+//
+//  EfficacyView.swift
+//  Pokedex
+//
+//  Created by Freak on 03.04.2026.
+//
+
+
+import SwiftUI
+
+struct EfficacyView: View {
+    let title: String
+    let efficacies: [(String, Int)]
+    private let spacing: Int = 10
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: CGFloat(spacing)) {
+            Text(title)
+                .font(.title2)
+
+                .bold()
+            VStack {
+                HStack {
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(efficacies, id: \.1) { type in
+                                HStack {
+                                    AsyncImage(url: getIconUrl(for: type.1))
+                                    Text(type.0)
+                                }
+                            }
+                        }
+                    }.scrollIndicators(.hidden)
+
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+
+    func getIconUrl(for id: Int) -> URL? {
+        URL(
+            string:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/small/\(id).png"
+        )
+    }
+}
