edc049123d9af7373f9166cea26d8c90104e4b09
Erik M <muzsi.erik@agilefreaks.com>
2026-04-08 13:32:49 +0300

    Fix audio player recreation

--- Pokedex/ViewModel/SoundManager.swift
+++ Pokedex/ViewModel/SoundManager.swift
@@ -1,4 +1,3 @@
-import AVFoundation
 //
 //  SoundManager.swift
 //  Pokedex
@@ -6,39 +5,48 @@ import AVFoundation
 //  Created by Freak on 03.04.2026.
 //
 import SwiftUI
+import AVFoundation
 
-@Observable
 class SoundManager {
-    var audioPlayer: AVPlayer?
+    let audioPlayer = AVPlayer()
+    
+    init() {
+        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        try? AVAudioSession.sharedInstance().setActive(true)
+    }
 
     func playCry(name: String) {
-        let urlString = PokedexStrings.getPokemonCryURLString(for: name.lowercased())
+        let urlString = PokedexStrings.getPokemonCryURLString(
+            for: name.lowercased()
+        )
 
         guard let url = URL(string: urlString) else { return }
 
         let playerItem = AVPlayerItem(url: url)
-        audioPlayer = AVPlayer(playerItem: playerItem)
-        audioPlayer?.play()
+        audioPlayer.replaceCurrentItem(with: playerItem)
+        audioPlayer.play()
     }
 
-    func canPlaySound(of name: String)async -> Bool {
-        let urlString = PokedexStrings.getPokemonCryURLString(for: name.lowercased())
-        
-        guard let url = URL(string:urlString) else { return false }
+    func canPlaySound(of name: String) async -> Bool {
+        let urlString = PokedexStrings.getPokemonCryURLString(
+            for: name.lowercased()
+        )
+
+        guard let url = URL(string: urlString) else { return false }
         var request = URLRequest(url: url)
         request.httpMethod = "HEAD"
         request.timeoutInterval = 5.0
 
         do {
-                let (_, response) = try await URLSession.shared.data(for: request)
-                
-                if let httpResponse = response as? HTTPURLResponse {
-                    return httpResponse.statusCode == 200
-                }
-            } catch {
-                return false
+            let (_, response) = try await URLSession.shared.data(for: request)
+
+            if let httpResponse = response as? HTTPURLResponse {
+                return httpResponse.statusCode == 200
             }
-            
+        } catch {
             return false
+        }
+
+        return false
     }
 }
