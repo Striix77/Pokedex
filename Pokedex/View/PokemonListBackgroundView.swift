1fcdba904f7fe9da8f107f508bb4d37395f5b6f1
Erik M <muzsi.erik@agilefreaks.com>
2026-04-15 17:35:39 +0300

    Create a basic gradient for list background

--- Pokedex/View/PokemonListBackgroundView.swift
+++ Pokedex/View/PokemonListBackgroundView.swift
@@ -8,7 +8,18 @@ import SwiftUI
 
 struct PokemonListBackgroundView: View {
     var body: some View {
-        
+        LinearGradient(
+            stops: [
+                Gradient.Stop(
+                    color: Color(red: 0.898, green: 0.4196, blue: 0.4275),
+                    location: 0.0
+                ),
+                Gradient.Stop(color: Color(red: 0.4471, green: 0.1529, blue: 0.2784), location: 1.0)
+            ],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+        .ignoresSafeArea()
     }
 }
 
