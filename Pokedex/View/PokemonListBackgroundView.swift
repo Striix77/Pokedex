ac6d4d96caa70e816b25dd95fa91e2efb83bd801
Erik M <muzsi.erik@agilefreaks.com>
2026-04-16 16:57:58 +0300

    Add Pokeball Icon to list background

--- Pokedex/View/PokemonListBackgroundView.swift
+++ Pokedex/View/PokemonListBackgroundView.swift
@@ -20,10 +20,34 @@ struct PokemonListBackgroundView: View {
             endPoint: .bottomTrailing
         )
         .ignoresSafeArea()
+        ZStack{
+            VStack{
+                HStack {
+                    Spacer()
+                    Image("PokeballIcon")
+                        .resizable()
+                        .frame(width:280, height:180)
+                        .opacity(0.3)
+                        .rotationEffect(Angle(degrees: -30))
+                        
+                }
+                .padding(.top, 75)
+                Spacer()
+            }
+        }
     }
 }
 
 #Preview {
     @Previewable @State var viewModel = PokemonViewModel()
     PokemonListView(viewModel: viewModel)
+}
+#Preview {
+    PokemonListBackgroundView(stops: [
+        Gradient.Stop(
+            color: Color(red: 0.898, green: 0.4196, blue: 0.4275),
+            location: 0.0
+        ),
+        Gradient.Stop(color: Color(red: 0.4471, green: 0.1529, blue: 0.2784), location: 1.0)
+    ])
 }
