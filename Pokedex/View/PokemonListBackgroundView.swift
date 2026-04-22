1822d6e15a550aca5fd202233316003500819703
Erik M <muzsi.erik@agilefreaks.com>
2026-04-22 16:28:06 +0300

    Create animation for list background view

--- Pokedex/View/PokemonListBackgroundView.swift
+++ Pokedex/View/PokemonListBackgroundView.swift
@@ -7,26 +7,38 @@
 import SwiftUI
 
 struct PokemonListBackgroundView: View {
+    @State private var animateGradient = false
     var stops: [Gradient.Stop]
-    
+
     var body: some View {
-        ZStack{
+        ZStack {
             LinearGradient(
                 stops: stops,
-                startPoint: .topLeading,
+                startPoint: animateGradient ? .topLeading : UnitPoint(x: UnitPoint.topLeading.x + 0.2, y: UnitPoint.topLeading.y + 0.2),
                 endPoint: .bottomTrailing
             )
             .ignoresSafeArea()
-            ZStack{
-                    Image("PokeballIcon")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:350)
-                        .opacity(0.3)
-                        .rotationEffect(Angle(degrees: -30))
-               
+            .onAppear {
+                withAnimation(
+                    .easeInOut(duration: 30).repeatForever(autoreverses: true)
+                ) {
+                    animateGradient.toggle()
+                }
             }
-            .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topTrailing)
+            ZStack {
+                Image("PokeballIcon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 350)
+                    .opacity(0.3)
+                    .rotationEffect(Angle(degrees: -30))
+
+            }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .topTrailing
+            )
             .offset(x: 50)
         }
     }
@@ -43,6 +55,13 @@ struct PokemonListBackgroundView: View {
             color: Color.listViewBackground1,
             location: 0.0
         ),
-        Gradient.Stop(color: Color.listViewBackground2, location: 1.0)
+        Gradient.Stop(
+            color: Color.listViewBackground2,
+            location: 0.4
+        ),
+        Gradient.Stop(
+            color: Color.listViewBackground2,
+            location: 1.0
+        ),
     ])
 }
