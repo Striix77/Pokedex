62eee6fe70b29fb8ed595c31fcbda6040e498819
Erik M <muzsi.erik@agilefreaks.com>
2026-04-29 16:01:39 +0300

    Create animated glowing ring

--- Pokedex/Presentation/PokemonList/View/Components/PokemonViews/PokemonGridView.swift
+++ Pokedex/Presentation/PokemonList/View/Components/PokemonViews/PokemonGridView.swift
@@ -73,6 +73,35 @@ struct PokemonGridCard: View {
         .frame(maxWidth: .infinity)
         .padding(.bottom)
         .foregroundStyle(.white)
+struct GlowingRing: View {
+    let colors: (Color?, Color?)
+    @State private var rotation: Double = 0
+    @State private var scale: Double = 0
+    @State private var opacity: Double = 1
+
+    var body: some View {
+        let color1 = colors.0 ?? .white
+        let color2 = colors.1 ?? color1
+
+        Circle()
+            .stroke(
+                AngularGradient(
+                    colors: [color1, color2, color1],
+                    center: .center
+                ),
+                lineWidth: 16
+            )
+            .blur(radius: 28)
+            .rotationEffect(.degrees(rotation))
+            .scaleEffect(scale)
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 5).repeatForever(autoreverses: false)) {
+                    rotation = 360
+                    scale = 3
+                    opacity = 0
+                }
+            }
     }
 }
 
