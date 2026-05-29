fe08dca024a5139b3f3358e038c1e75fcda856c1
Erik M <muzsi.erik@agilefreaks.com>
2026-05-29 13:26:12 +0300

    Create custom animation for favorite button

--- Pokedex/Presentation/PokemonDetails/View/Components/FavoritePokemonButton.swift
+++ Pokedex/Presentation/PokemonDetails/View/Components/FavoritePokemonButton.swift
@@ -8,24 +8,47 @@ import SwiftUI
 
 struct FavoritePokemonButton: View {
     @Environment(\.favoritesService) var favoritesManager
+    @State private var overlayScale: CGFloat = 1
+    @State private var overlayOpacity: Double = 1
+    @State private var animationDuration: Double = 0.5
+
     let id: Int
+
+    private let finalOverlayScale: CGFloat = 3
+    private let finalOverlayOpacity: Double = 0
+    private let initialOverlayScale: CGFloat = 1
+    private let initialOverlayOpacity: Double = 1
 
     private var isFavorite: Bool {
         favoritesManager.favoriteIDs.contains(id)
     }
 
     var body: some View {
-        Button {
-            favoritesManager.toggle(id)
-        } label: {
-            Image(
-                systemName: isFavorite
-                    ? "heart.fill" : "heart"
-            )
-            .font(.system(size: 30))
-            .foregroundStyle(isFavorite ? .red : .gray)
+        Image(
+            systemName: isFavorite
+                ? "heart.fill" : "heart"
+        )
+        .font(.system(size: 30))
+        .foregroundStyle(isFavorite ? Color.accentColor : .gray)
+        .overlay(
+            Image(systemName: "heart")
+                .font(.system(size: 30))
+                .foregroundStyle(isFavorite ? Color.accentColor : .gray)
+                .scaleEffect(overlayScale)
+                .opacity(overlayOpacity)
+        )
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: animationDuration)) {
+                if !isFavorite {
+                    overlayScale = finalOverlayScale
+                    overlayOpacity = finalOverlayOpacity
+                }
+                favoritesManager.toggle(id)
+            } completion: {
+                overlayScale = initialOverlayScale
+                overlayOpacity = initialOverlayOpacity
+            }
         }
-        .buttonStyle(.borderless)
     }
 }
 
