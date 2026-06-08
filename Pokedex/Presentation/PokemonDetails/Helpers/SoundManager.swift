b4fa4fc7c68c847413956dff92fc8a59eb90791f
Erik M <muzsi.erik@agilefreaks.com>
2026-06-08 15:16:15 +0300

    Set audio mode to ambient

--- Pokedex/Presentation/PokemonDetails/Helpers/SoundManager.swift
+++ Pokedex/Presentation/PokemonDetails/Helpers/SoundManager.swift
@@ -4,16 +4,16 @@
 //
 //  Created by Freak on 03.04.2026.
 //
-import Foundation
 import AVFoundation
+import Foundation
 
 @Observable
 class SoundManager {
     let audioPlayer = AVPlayer()
-    private var canPlayCache: [String:Bool] = [:]
-    
+    private var canPlayCache: [String: Bool] = [:]
+
     init() {
-        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        try? AVAudioSession.sharedInstance().setCategory(.ambient, mode: .default)
         try? AVAudioSession.sharedInstance().setActive(true)
     }
 
@@ -28,8 +28,8 @@ class SoundManager {
         audioPlayer.replaceCurrentItem(with: playerItem)
         audioPlayer.play()
     }
-    
-    private func checkSoundExistence(of name: String) async -> Bool{
+
+    private func checkSoundExistence(of name: String) async -> Bool {
         let urlString = PokedexStrings.getPokemonCryURLString(
             for: name.lowercased()
         )
@@ -37,19 +37,18 @@ class SoundManager {
         var request = URLRequest(url: url)
         request.httpMethod = "HEAD"
         request.timeoutInterval = 5.0
-        
+
         let response = try? await URLSession.shared.data(for: request).1
         return (response as? HTTPURLResponse)?.statusCode == 200
-
     }
 
     func canPlaySound(of name: String) async -> Bool {
         if let cachedResult = canPlayCache[name] {
             return cachedResult
         }
-        
+
         let result = await checkSoundExistence(of: name)
-        
+
         canPlayCache[name] = result
         return result
     }
